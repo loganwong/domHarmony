4c73576949b7e6cbc87ff39bf011c6574c66f10d
BRIAN   AND DONNA WONG <loganwong@student.midpac.edu>
2018-08-27 21:23:24 -1000

    Finished numbers 1-8

--- js/app.js
+++ js/app.js
@@ -1,21 +1,31 @@
 /*Hey Prepsters! Let's help Ronald McDonald find true love by revising his online dating profile!*/
 
 //1. In the nav element, target the span element with the id of 'matCount' and change the number from 0 to 11.
-
+matCount.innerHTML = 11;
 //2. In the nav element, target the span element with the id of 'msgCount' and change the number from 0 to 23.
-
+msgCount.innerHTML = 23;
 //3. In the div id data section, target the div element with the id of 'fullname' and change Full Name to Ronald McDonald.
-
+fullname.innerHTML = "Ronald McDonald";
 //4. In the div id data section, target the div element with the id of 'age' and change Age to 63 Years Old.
-
+age.innerHTML = '63 years old';
 //5. In the div id data section, create a div with an id of job and give it the following information: Clown and Restauranteur.
-
+var jobBox = document.createElement('div');
+jobBox = 'job';
+jobBox.innerHTML = "Clown and Restauranteur";
+data.appendChild(jobBox);
 //6. In the div id data section, create a div with an id of hobbies and give it the following information: Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.
+var hobbieBox = document.createElement('div');
+hobbieBox.id = 'hobbies';
+hobbieBox.innerHTML = "Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.";
+data.appendChild(hobbies);
 
 //7. In the div id data section, create a div with an id of location and give it the following information: Honolulu, HI.
-
+var locationBox = document.createElement('div');
+locationBox.id = 'location';
+location.innerHTML = 'Honolulu, HI'
+data.appendChild(location);
 //8. In the div id data section, create a div with an id of wants and give it the following information: Looking for a Mrs. McDonald.
-
+var wantBox = document.createElement('div');
 //9. In the div id profile section, create a paragraph element with an id of pro2 and create a your own profile for Ronald (at least 2 sentences).
 
 //10. In the div id matches section, target the first div element with the class name of firstName and change Name to Wendy.
@@ -33,7 +43,7 @@
  //div with class name of firstName
  //div with class name of otherAge
  //div with class name of status
- //div with class anem of Motto
+ //div with class name of Motto
 
 
 
